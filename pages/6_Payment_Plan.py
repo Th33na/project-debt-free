42ce2a58b968fd83341366ce529fcae0bd9b76c2
Theena Dang <maria.cristina.dang@gmail.com>
2022-08-14 21:57:59 -0700

    fix bug and name

--- pages/6_Payment_Plan.py
+++ pages/6_Payment_Plan.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import streamlit as st
-from db_utils import get_debt_free_db_engine, get_transaction_for_user_year
+from utils.db_utils import get_debt_free_db_engine, get_transaction_for_user_year
 
 st.set_page_config(
     page_title="Debt Free Payment Plan",
